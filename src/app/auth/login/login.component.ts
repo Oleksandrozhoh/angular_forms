4b35f3868989890239564726646147bd03c294fe
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2025-06-28 19:22:46 -0500

    template driven form complete

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +11,30 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+
+      // Restore cached email from localStorage if available
+      const cachedEmail = window.localStorage.getItem('cached-user-email');
+      if (cachedEmail) {
+        setTimeout(() => {
+        this.form().controls['email'].setValue(cachedEmail)}, 1)
+      }
+
+      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) => {
+          window.localStorage.setItem('cached-user-email', value.email);
+        }},
+      )
+
+      this.destroyRef.onDestroy(() => {
+        subscription?.unsubscribe();
+      });
+    });
+  }
 
 onSubmit(formData: NgForm) {
   if(formData.form.invalid) {
@@ -18,6 +44,8 @@ onSubmit(formData: NgForm) {
     console.log('Password:', enteredPassword);
     console.log(formData);
   }
+
+  formData.form.reset();
 }
 
 
